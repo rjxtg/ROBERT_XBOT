e72b10b00cf204c73ccea9920ac3b5689becd422
RJ <147345876+rjxtg@users.noreply.github.com>
2024-11-13 23:22:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,9 +43,9 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 
 # MongoDB information
-SECONDDB_URI = environ.get('SECONDDB_URI', "mongodb+srv://you:you@dq.nfjr9.mongodb.net/?retryWrites=true&w=majority&appName=DQ")
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://me:me@rj.jttqy.mongodb.net/?retryWrites=true&w=majority&appName=RJ")
-DATABASE_NAME = environ.get('DATABASE_NAME', "RJ")
+SECONDDB_URI = environ.get('SECONDDB_URI', "mongodb+srv://ROCK:ROCK@cluster0.bjpan.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://RobertBot:RobertBot>@cluster0.de1opei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
