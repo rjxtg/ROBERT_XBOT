04a295f11a3020415838cd55dc5f63b89a6760b2
RJ <147345876+rjxtg@users.noreply.github.com>
2024-11-30 15:05:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -130,7 +130,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[❑{get_size(file.file_size)}] ⟹ {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"❑{get_size(file.file_size)} ⟹ {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -139,7 +139,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"❑{get_size(file.file_size)} ⟹ {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
                 InlineKeyboardButton(
                     text=f"{get_size(file.file_size)}",
