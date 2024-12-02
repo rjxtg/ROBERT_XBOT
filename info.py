e9f250083c36f3f6ae4bad665ec1c31163db9f48
RJ <147345876+rjxtg@users.noreply.github.com>
2024-12-02 18:37:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -46,7 +46,7 @@ NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 SECONDDB_URI = environ.get('SECONDDB_URI', "mongodb+srv://ROCK:ROCK@cluster0.bjpan.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://RobertBot:RobertBot@cluster0.de1opei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'CK_files')
 
 # Others
 IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'False')), False)
